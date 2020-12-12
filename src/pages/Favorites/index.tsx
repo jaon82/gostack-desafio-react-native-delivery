452647420a70a832f8917bd3e11a539fc1211337
Jean Nunes <jaon@itriad.org.br>
2020-12-12 19:16:56 -0400

    Listar os pratos favoritos da sua API

--- src/pages/Favorites/index.tsx
+++ src/pages/Favorites/index.tsx
@@ -32,7 +32,12 @@ const Favorites: React.FC = () => {
 
   useEffect(() => {
     async function loadFavorites(): Promise<void> {
-      // Load favorite foods from api
+      const response = await api.get<Food[]>('favorites');
+      const foodsParsed = response.data.map(food => ({
+        ...food,
+        formattedPrice: formatValue(food.price),
+      }));
+      setFavorites(foodsParsed);
     }
 
     loadFavorites();
